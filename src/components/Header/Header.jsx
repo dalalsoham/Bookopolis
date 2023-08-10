1823b44914f1898825752360e3a4fd3dd8465c4b
mr_sd_jack_003 <barson0habra@gmail.com>
2023-08-11 01:32:39 +0530

    done with header,navbar(half)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+import SearchForm from '../SearchForm/SearchForm';
+import "./Header.css";
 
-function Header() {
+const Header = () => {
   return (
-    <div>
-      
+    <div className="holder">
+      <header className="header">
+        <Navbar />
+        <div className="header-content flex flex-c text-center text-white">
+          <h2 className="header-title text-capitalize">
+            Find your book of choice.
+          </h2><br />
+          <p className="header-text fs-18 fw-3">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+          </p>
+          <SearchForm />
+        </div>
+      </header>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -4,12 +4,19 @@ import {
   BrowserRouter, Routes, Route
 } from 'react-router-dom';
 import './index.css';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import BookList from './components/BookList/BookList';
+import BookDetails from './components/BookDetails/BookDetails';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
-      
+      <Route path = "/" element = {<Home />} />
+      <Route path = "about" element = {<About />} />
+      <Route path = "book" element = {<BookList />} />
+      <Route path = "/book/:id" element = {<BookDetails />} />
     </Routes>
   </BrowserRouter>
 );
