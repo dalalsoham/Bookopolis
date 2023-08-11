4d3bdb6ece66e6e87a5f6d8104a6883d8ced55fe
mr_sd_jack_003 <barson0habra@gmail.com>
2023-08-12 00:10:56 +0530

    half done with navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,20 @@
-import React, {useStyles} from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import "./Navbar.css";
 import logoImg from "../../images/logo.png";
 import {HiOutlineMenuAlt3} from "react-icons/hi";
+// import { handleNavbar, toggleMenu } from './helpers';
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const handleNavbar = () => setToggleMenu(!toggleMenu);
   return (
     <nav className='navbar' id = "navbar">
       <div className='container navbar-content flex'>
         <div className='brand-and-toggler flex flex-sb'>
           <Link to = "/" className='navbar-brand flex'>
             <img src = {logoImg} alt = "site logo" />
-            <span className='text-uppercase fw-7 fs-24 ls-1'>bookhub</span>
+            <span className='text-uppercase fw-7 fs-14 ls-2'>Bookopolis</span>
           </Link>
           <button type = "button" className='navbar-toggler-btn' onClick={handleNavbar}>
             <HiOutlineMenuAlt3 size = {35} style = {{
