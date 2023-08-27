3db214ae55bbdf4c3408c312e582161b1de1c938
mr_sd_jack_003 <barson0habra@gmail.com>
2023-08-28 02:55:41 +0530

    loader done

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import LoaderImg from "../../images/loader.svg";
+import "./Loader.css";
 
-function Loader() {
+const Loader = () => {
   return (
-    <div>
-      
+    <div className="oader flex flex-c">
+      <img src = {LoaderImg} alt = "loader" />
     </div>
   )
 }
